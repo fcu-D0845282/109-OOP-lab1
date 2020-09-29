ed7818bcc0640f123de14159b34019f972c339b8
user <user@EA04>
2020-09-29 09:37:38 +0800

    123

--- src/lab1/Haha.java
+++ src/lab1/Haha.java
@@ -4,7 +4,9 @@ public class Haha {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("I'm ¦¿©÷ÂE")
+		System.out.println("My nid is D0845282");
+		System.out.println("This is lab 1 hw");
 	}
 
 }
