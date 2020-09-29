4f86dc139ebb4f209eadb3268fac7a61fda26878
fcu-D0845282 <72053378+fcu-D0845282@users.noreply.github.com>
2020-09-29 09:51:49 +0800

    Update Haha.java

--- src/lab1/Haha.java
+++ src/lab1/Haha.java
@@ -4,7 +4,7 @@ public class Haha {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm ¦¿©÷ÂE")
+		System.out.println("I'm chang hong")
 		System.out.println("My nid is D0845282");
 		System.out.println("This is lab 1 hw");
 	}
